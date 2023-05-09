279a9244245575cee20395cf3c3b32d555bd5eac
Gabriel <gabrigomez15@gmail.com>
2023-05-09 15:54:51 -0300

    feat: add first route and home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import { Home } from './components/Home';
 import './App.css';
 
 function App() {
 
   return (
     <main>
-            
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>            
     </main>
   )
 }
