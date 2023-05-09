8585487e4c73272eb1f4af26251f5d9ad56c5e84
Gabriel <gabrigomez15@gmail.com>
2023-05-09 16:27:37 -0300

    feat: start function to call api on home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,16 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 
 export const Home = () => {
+  useEffect(() => {
+    fetch('https://abum-rebiew-api.onrender.com/api/v1/albums')
+      .then((response) => {
+        if(response.ok) {
+          console.log(response);
+        }
+        throw new Error('Something went wrong');
+      })
+  })
+
   return (
     <div className='bg-cyan-300'>
       <p>Home</p>
