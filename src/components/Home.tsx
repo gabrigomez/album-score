b475310d9d9218ec8e976a6b2274d1ecb82c4854
Gabriel <gabrigomez15@gmail.com>
2023-05-10 11:05:51 -0300

    feat: add albums hooks in home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Home = () => {
-  useEffect(() => {
-    const fetchAlbums = async() => {
-      const response = await fetch('https://abum-rebiew-api.onrender.com/api/v1/albums');
-      const data = await response.json();
-      if(response.ok) {
-        console.log(data);
-      }
+  const [albums, setAlbums] = useState([]);
+  console.log(albums);
+
+  const fetchAlbums = async() => {
+    const response = await fetch('https://abum-rebiew-api.onrender.com/api/v1/albums');
+    const data = await response.json();
+    
+    if(response.ok && albums.length === 0) {
+      setAlbums(data);
     }
+  };
+
+  useEffect(() => {
     fetchAlbums();
-  })
+  });
 
   return (
     <div className='flex justify-center'>
