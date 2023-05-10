0dc70fcdcae4f0c5839ce4351a676e15891b4bc1
Gabriel <gabrigomez15@gmail.com>
2023-05-10 10:18:34 -0300

    feat: add styles to NavBar and fix index.css

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <nav className='mb-4'>
-      Navbar
+    <nav className='mb-4 mt-2 flex justify-between'>
+      <div className='ml-4'>
+        <Link to={'/'} className='text-lg font-semibold'>
+          ALBUM REVIEW
+        </Link>
+      </div>
+      <div className='flex mr-4'>
+        <div className='mr-2'>
+          <p>about</p>
+        </div>
+        <div>
+          <p>add album</p>
+        </div>
+      </div>
     </nav>
   )
 }
